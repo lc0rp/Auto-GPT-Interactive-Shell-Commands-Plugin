fab5576086498340a500f09296291b41bb737481
Luke K <2609441+lc0rp@users.noreply.github.com>
2023-05-27 17:43:15 +0000

    Added timeout, updated tests

--- src/autogpt_interactive_shell_commands_plugin/interactive_shell_commands.py
+++ src/autogpt_interactive_shell_commands_plugin/interactive_shell_commands.py
@@ -1,163 +1,203 @@
 """Execute interactive shell commands in the workspace"""
-from calendar import c
 import os
 import subprocess
 import sys
 
 
-def execute_interactive_shell_linux(command_line: str) -> list[dict]:
-    """Execute a shell command that requires interactivity and return the output.
+class InteractiveShellCommands:
+    def __init__(self, default_timeout_seconds) -> None:
+        self._default_timeout_seconds = default_timeout_seconds
 
-    Args:
-        command_line (str): The command line to execute
+    def execute_interactive_shell(
+        self, command_line: str, timeout_seconds: int = None
+    ) -> list[dict]:
+        """Execute a shell command that requires interactivity and return the output.
 
-    Returns:
-        list[dict]: The interaction between the user and the process, as a list of dictionaries: [{role: "user"|"process"|"error", content: "the content of the interaction"}, ...]
-    """
-    import select
+        Args:
+            command_line (str): The command line to execute
+            timeout_seconds (int): The timeout in seconds
 
-    process = subprocess.Popen(
-        command_line,
-        shell=True,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
+        Returns:
+            list[dict]: The interaction between the user and the process, as a list of dictionaries: [{role: "user"|"process"|"error", content: "the content of the interaction"}, ...]
+        """
+        if timeout_seconds is None:
+            timeout_seconds = self._default_timeout_seconds
 
-    # To capture the conversation, we'll read from one set of descriptors, save the output and write it to the other set descriptors.
-    fd_map = {
-        process.stdout.fileno(): ("process", sys.stdout.buffer),
-        process.stderr.fileno(): ("error", sys.stderr.buffer),
-        sys.stdin.fileno(): ("user", process.stdin),  # Already buffered
-    }
+        if sys.platform == "win32":
+            return self.execute_interactive_shell_crossplatform(
+                command_line, timeout_seconds
+            )
+        else:
+            return self.execute_interactive_shell_linux(command_line, timeout_seconds)
 
-    conversation = []
+    def execute_interactive_shell_linux(
+        self, command_line: str, timeout_seconds: int = None
+    ) -> list[dict]:
+        """Execute a shell command that requires interactivity and return the output.
 
-    while True:
-        read_fds, _, _ = select.select(list(fd_map.keys()), [], [])
-        input_fd = next(fd for fd in read_fds if fd in fd_map)
-        role, output_buffer = fd_map[input_fd]
+        Args:
+            command_line (str): The command line to execute
+            timeout_seconds (int): The timeout in seconds
 
-        input_buffer = os.read(input_fd, 1024)
-        if input_buffer == b"":
-            break
-        output_buffer.write(input_buffer)
-        output_buffer.flush()
-        content = input_buffer.decode("utf-8")
-        content = (
-            content.replace("\r", "").replace("\n", " ").strip() if content else ""
+        Returns:
+            list[dict]: The interaction between the user and the process, as a list of dictionaries: [{role: "user"|"process"|"error", content: "the content of the interaction"}, ...]
+        """
+        import select
+
+        if timeout_seconds is None:
+            timeout_seconds = self._default_timeout_seconds
+
+        process = subprocess.Popen(
+            command_line,
+            shell=True,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
         )
-        conversation.append({"role": role, "content": content})
 
-    process.wait()
-    process.stdin.close()
-    process.stdout.close()
-    process.stderr.close()
+        # To capture the conversation, we'll read from one set of descriptors, save the output and write it to the other set descriptors.
+        fd_map = {
+            process.stdout.fileno(): ("process", sys.stdout.buffer),
+            process.stderr.fileno(): ("error", sys.stderr.buffer),
+            sys.stdin.fileno(): ("user", process.stdin),  # Already buffered
+        }
 
-    return conversation
+        conversation = []
 
+        while True:
+            read_fds, _, _ = select.select(list(fd_map.keys()), [], [])
+            input_fd = next(fd for fd in read_fds if fd in fd_map)
+            role, output_buffer = fd_map[input_fd]
 
-def execute_interactive_shell_crossplatform(command_line: str) -> list[dict]:
-    """Execute a shell command that requires interactivity and return the output.
-    This can also work on linux, but is less native than the other function.
+            input_buffer = os.read(input_fd, 1024)
+            if input_buffer == b"":
+                break
+            output_buffer.write(input_buffer)
+            output_buffer.flush()
+            content = input_buffer.decode("utf-8")
+            content = (
+                content.replace("\r", "").replace("\n", " ").strip() if content else ""
+            )
+            conversation.append({"role": role, "content": content})
 
-    Args:
-        command_line (str): The command line to execute
+        try:
+            process.wait(timeout=timeout_seconds)
+            process.stdin.close()
+            process.stdout.close()
+            process.stderr.close()
+        except subprocess.TimeoutExpired:
+            conversation.append(
+                {"role": "error", "content": f"Timeout after {timeout_seconds} seconds"}
+            )
 
-    Returns:
-        list[dict]: The interaction between the user and the process, as a list of dictionaries: [{role: "user"|"process"|"error", content: "the content of the interaction"}, ...]
-    """
-    from sarge import Capture, Command
+        return conversation
 
-    command = Command(
-        command_line,
-        stdout=Capture(buffer_size=1),
-        stderr=Capture(buffer_size=1),
-    )
-    command.run(input=subprocess.PIPE, async_=True)
+    def execute_interactive_shell_crossplatform(
+        self, command_line: str, timeout_seconds: int = None
+    ) -> list[dict]:
+        """Execute a shell command that requires interactivity and return the output.
+        This can also work on linux, but is less native than the other function.
 
-    # To capture the conversation, we'll read from one set of descriptors, save the output and write it to the other set descriptors.
-    fd_map = {
-        command.stdout: ("process", sys.stdout.buffer),
-        command.stderr: ("error", sys.stderr.buffer),
-    }
+        Args:
+            command_line (str): The command line to execute
+            timeout_seconds (int): The timeout in seconds
 
-    conversation = []
+        Returns:
+            list[dict]: The interaction between the user and the process, as a list of dictionaries: [{role: "user"|"process"|"error", content: "the content of the interaction"}, ...]
+        """
+        if timeout_seconds is None:
+            timeout_seconds = self._default_timeout_seconds
 
-    while True:
-        output = {fd: fd.read(timeout=0.1) for fd in fd_map.keys()}
-        if not any(output.values()):
-            break
+        from sarge import Capture, Command
 
-        content = ""
-        for fd, output_content in output.items():
-            if output_content:
-                output_content = (
-                    output_content + b"\n"
-                    if not output_content.endswith(b"\n")
-                    else output_content
-                )
-                fd_map[fd][1].write(output_content)
-                fd_map[fd][1].flush()
+        command = Command(
+            command_line,
+            stdout=Capture(buffer_size=1),
+            stderr=Capture(buffer_size=1),
+        )
+        command.run(input=subprocess.PIPE, async_=True)
 
-                content = output_content.decode("utf-8")
-                content = (
-                    content.replace("\r", "").replace("\n", " ").strip()
-                    if content
-                    else ""
-                )
-                conversation.append({"role": fd_map[fd][0], "content": content})
+        # To capture the conversation, we'll read from one set of descriptors, save the output and write it to the other set descriptors.
+        fd_map = {
+            command.stdout: ("process", sys.stdout.buffer),
+            command.stderr: ("error", sys.stderr.buffer),
+        }
 
-        if any(output.values()):
-            prompt = "Response [None]: "
-            os.write(sys.stdout.fileno(), prompt.encode("utf-8"))
-            stdin = os.read(sys.stdin.fileno(), 1024)
-            if stdin != b"":
-                try:
-                    command.stdin.write(stdin)
-                    command.stdin.flush()
-                    content = stdin.decode("utf-8")
+        conversation = []
+
+        while True:
+            output = {fd: fd.read(timeout=0.1) for fd in fd_map.keys()}
+            if not any(output.values()):
+                break
+
+            content = ""
+            for fd, output_content in output.items():
+                if output_content:
+                    output_content = (
+                        output_content + b"\n"
+                        if not output_content.endswith(b"\n")
+                        else output_content
+                    )
+                    fd_map[fd][1].write(output_content)
+                    fd_map[fd][1].flush()
+
+                    content = output_content.decode("utf-8")
                     content = (
                         content.replace("\r", "").replace("\n", " ").strip()
                         if content
                         else ""
                     )
-                    conversation.append({"role": "user", "content": content})
-                except (BrokenPipeError, OSError):
-                    # Child process already exited
-                    print("Command exited... returning.")
+                    conversation.append({"role": fd_map[fd][0], "content": content})
 
-    command.wait()
-    return conversation
+            if any(output.values()):
+                prompt = "Response [None]: "
+                os.write(sys.stdout.fileno(), prompt.encode("utf-8"))
+                stdin = os.read(sys.stdin.fileno(), 1024)
+                if stdin != b"":
+                    try:
+                        command.stdin.write(stdin)
+                        command.stdin.flush()
+                        content = stdin.decode("utf-8")
+                        content = (
+                            content.replace("\r", "").replace("\n", " ").strip()
+                            if content
+                            else ""
+                        )
+                        conversation.append({"role": "user", "content": content})
+                    except (BrokenPipeError, OSError):
+                        # Child process already exited
+                        print("Command exited... returning.")
 
+        try:
+            command.wait(timeout=timeout_seconds)
+        except subprocess.TimeoutExpired:
+            conversation.append(
+                {"role": "error", "content": f"Timeout after {timeout_seconds} seconds"}
+            )
 
-def ask_user(prompts: list[str]) -> list[str]:
-    """
-    Ask the user a series of prompts and return the responses
+        return conversation
 
-    Args:
-        prompts (list[str]): The prompts to ask the user
+    def ask_user(self, prompts: list[str], timeout_seconds: int = None) -> list[str]:
+        """
+        Ask the user a series of prompts and return the responses
 
-    Returns:
-        list[str]: The responses from the user
-    """
-    results = []
-    for prompt in prompts:
-        response = input(prompt)
-        results.append(response)
-    return results
+        Args:
+            prompts (list[str]): The prompts to ask the user
 
+        Returns:
+            list[str]: The responses from the user
+        """
+        if timeout_seconds is None:
+            timeout_seconds = self._default_timeout_seconds
 
-def execute_interactive_shell(command_line: str) -> list[dict]:
-    """Execute a shell command that requires interactivity and return the output.
+        from inputimeout import TimeoutOccurred, inputimeout
 
-    Args:
-        command_line (str): The command line to execute
+        results = []
+        try:
+            for prompt in prompts:
+                response = inputimeout(prompt, timeout=timeout_seconds)
+                results.append(response)
+        except TimeoutOccurred:
+            results.append(f"Timeout after {timeout_seconds} seconds")
 
-    Returns:
-        list[dict]: The interaction between the user and the process, as a list of dictionaries: [{role: "user"|"process"|"error", content: "the content of the interaction"}, ...]
-    """
-    if sys.platform == "win32":
-        return execute_interactive_shell_crossplatform(command_line)
-    else:
-        return execute_interactive_shell_linux(command_line)
+        return results
